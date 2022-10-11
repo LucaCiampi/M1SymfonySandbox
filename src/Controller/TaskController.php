932013661d2fa2ff2e18d989c7eacf7ba6a1f091
Luca <luca.ciampi@edu.gobelins.fr>
2022-10-11 15:38:17 +0200

    added form

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Task;
+use App\Form\Type\TaskType;
+
+class TaskController extends AbstractController
+{
+    #[Route('/form', name: 'app_form')]
+    public function new(Request $request): Response
+    {
+        // just set up a fresh $task object (remove the example data)
+        $task = new Task();
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/success', name: 'form_success')]
+    public function renderSuccess(): Response
+    {
+        return $this->render('task/success.html.twig');
+    }
+}
